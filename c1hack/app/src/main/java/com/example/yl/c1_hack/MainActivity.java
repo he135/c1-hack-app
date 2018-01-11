5ea51b068565aa729780850a4e55ac84f496b0ef
Yiling Kao <ylkao@berkeley.edu>
2018-01-11 14:06:04 -0500

    starter project with tasks

---
+++ c1hack/app/src/main/java/com/example/yl/c1_hack/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.yl.c1_hack;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.task_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, TaskSetupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
---
+++ c1hack/app/src/main/java/com/example/yl/c1_hack/TaskSetupActivity.java
@@ -0,0 +1,13 @@
+package com.example.yl.c1_hack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaskSetupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_setup);
+    }
+}
